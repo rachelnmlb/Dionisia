fdd08bcf09b7ba9cd0c7e715814c5a0ab01a4162
Rachel Barros <rachelnmlb@hotmail.com>
2022-01-30 11:51:58 -0300

    first commit

---
+++ app/src/main/java/com/rachel/dionisia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rachel.dionisia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_inicio)
+    }
+}
